e12e24c53fefc0141bcc59f4f7a29cf578172f90
Failzuma <ibnuilham720@gmail.com>
2022-07-01 01:08:37 +0700

    first commit

---
+++ utbk.py
@@ -0,0 +1,20 @@
+def hitungTPS():
+  pu = float(input("Masukkan nilai Penalaran Umum: "))
+  km = float(input("Masukkan nilai Kemampuan Memahami Bacaan: "))
+  ppu = float(input("Masukkan nilai Pengetahuan dan Pemahaman Umum: "))
+  pk = float(input("Masukkan nilai Pengetahuan Kuantitatif: "))
+  averageTPS = (pu + km + ppu + pk) / 4
+  return(averageTPS)
+def hitungTKASAINTEK():
+  mtk = float(input("Masukkan nilai Matematika: "))
+  fsk = float(input("Masukkan nilai Fisika: "))
+  kma = float(input("Masukkan nilai Kimia: "))
+  bio = float(input("Masukkan nilai Biologi: "))
+  averageTKASAINTEK = (mtk + fsk + kma + bio) / 4
+  return(averageTKASAINTEK)
+def hitungBING():
+  bing = float(input("Masukkan nilai Bahasa Inggris: "))
+  return(bing)
+while True:
+  skorUTBK = (float(hitungTPS()+hitungTKASAINTEK()+hitungBING()) / 3)
+  print("Skor UTBK Anda:", skorUTBK)
